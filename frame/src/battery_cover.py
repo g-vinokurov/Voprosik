618ecac76ccdb1d953c0bc95de2d729956f16c56
g-vinokurov <g.vinokurov@g.nsu.ru>
2026-05-06 10:48:38 +0700

    Add Battery Cover

---
+++ frame/src/battery_cover.py
@@ -0,0 +1,82 @@
+# Код позаимствован из проекта робота T-1337
+# Форма АКБ вписывается в отсек для АКБ того проекта
+
+from build123d import *
+from ocp_vscode import show
+
+unit_size           = 8.0 # lego unit size
+unit_height         = 9.6 # lego brick height
+hole_inner_diameter = 4.8 # lego hole inner diameter
+hole_outer_diameter = 6.2 # lego hole outer diameter
+
+with BuildPart() as part:
+    length = unit_size * 20
+    width  = unit_size * 8
+    height = 3.2
+    Box(length, width, height)
+
+    x_pos = unit_size * -2.5
+    y_pos = 0
+    z_pos = 0
+    with BuildPart(Location((x_pos, y_pos, z_pos)), mode=Mode.SUBTRACT):
+        length = unit_size * 3
+        width  = unit_size * 6
+        height = unit_height
+        align  = (Align.CENTER, Align.CENTER, Align.CENTER)
+        Box(length, width, height)
+    
+    x_pos = unit_size * 7.5
+    y_pos = 0
+    z_pos = 0
+    with BuildPart(Location((x_pos, y_pos, z_pos)), mode=Mode.SUBTRACT):
+        length = unit_size * 3
+        width  = unit_size * 6
+        height = unit_height
+        align  = (Align.CENTER, Align.CENTER, Align.CENTER)
+        Box(length, width, height)
+    
+    x_pos = unit_size * -7.5
+    y_pos = 0
+    z_pos = 0
+    with BuildPart(Location((x_pos, y_pos, z_pos)), mode=Mode.SUBTRACT):
+        length = unit_size * 3
+        width  = unit_size * 6
+        height = unit_height
+        align  = (Align.CENTER, Align.CENTER, Align.CENTER)
+        Box(length, width, height)
+    
+    x_pos = unit_size * 2.5
+    y_pos = 0
+    z_pos = 0
+    with BuildPart(Location((x_pos, y_pos, z_pos)), mode=Mode.SUBTRACT):
+        length = unit_size * 3
+        width  = unit_size * 6
+        height = unit_height
+        align  = (Align.CENTER, Align.CENTER, Align.CENTER)
+        Box(length, width, height)
+
+    # Отверстия под крепежные винты
+    points = [
+        (unit_size * 0,    unit_size * 3.5,  0),
+        (unit_size * 0,    unit_size * -3.5, 0),
+        (unit_size * 7,    unit_size * 3.5,  0),
+        (unit_size * 7,    unit_size * -3.5, 0),
+        (unit_size * -7,   unit_size * 3.5,  0),
+        (unit_size * -7,   unit_size * -3.5, 0),
+        (unit_size * -9.5,               0,  0),
+        (unit_size * 9.5,                0,  0),
+    ]
+    for point in points:
+        with BuildPart(Location(point), mode=Mode.SUBTRACT):
+            radius = 1.5
+            height = 3.2
+            align  = (Align.CENTER, Align.CENTER, Align.CENTER)
+            Cylinder(radius, height, align=align)
+
+    roundable_edges = part.edges().filter_by(Axis.Z)
+    fillet(roundable_edges, radius=2)
+
+filename = f'{__file__.rstrip('.py')}'
+
+show(part.part, names=[filename])
+export_stl(part.part, f'{filename}.stl')
